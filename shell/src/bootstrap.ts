aa840f7fa8d36f480a720a1e97564b70f5e7f22c
Prachi Rajput <prajput@invessio.com>
2025-12-23 17:20:44 +0530

    config changes

--- shell/src/bootstrap.ts
+++ shell/src/bootstrap.ts
@@ -1,8 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes, withHashLocation())]
 })
   .catch((err) => console.error(err));
