b09815fe70970b05affc546043d1ee4dd4099acc
Prachi Rajput <prajput@invessio.com>
2025-12-23 16:55:25 +0530

    Auto-generated commit

---
+++ _angular_material.NQ9Bynw400.js
@@ -0,0 +1,1 @@
+var o=!0;export{o as \u0275\u0275tsModuleIndicatorApiExtractorWorkaround};
---
+++ main-WSBY7DDQ.js
@@ -0,0 +1,1 @@
+import{a as e}from"./chunk-OBBLAJGF.js";e("./assets/federation.manifest.json").catch(o=>console.error(o)).then(o=>import("./chunk-NJPIIV3Y.js")).catch(o=>console.error(o));
