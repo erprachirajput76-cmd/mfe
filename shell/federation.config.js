cc1574de7f33243635e16a6433736e40e53f576f
Prachi Rajput <prajput@invessio.com>
2025-12-23 16:51:57 +0530

    federation changes

--- shell/federation.config.js
+++ shell/federation.config.js
@@ -1,19 +1,13 @@
-const { withNativeFederation, shareAll } =
-  require('@angular-architects/native-federation/config');
+const { withNativeFederation, shareAll } = require('@angular-architects/native-federation/config');
 
 module.exports = withNativeFederation({
 
-  name: 'shell',
-
-  // 🔥 THIS FIXES file:///C:/ ISSUE FOR SHELL
-  publicPath: 'https://erprachirajput76-cmd.github.io/mfe/',
-
   remotes: {
-    mfe1: 'https://erprachirajput76-cmd.github.io/mfe/remoteEntry.json',
+    mfe1: 'https://erprachirajput76-cmd.github.io/mfe/remoteEntry.json'
   },
 
   shared: {
-    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
+    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' })
   },
 
 });
